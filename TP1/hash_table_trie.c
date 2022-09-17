fccd520eb7ce70728f4683c1f97a0fa4c8afa0d5
DesertPrince <lepetitprince10@gmx.fr>
2022-09-17 13:56:28 +0200

    Update

--- TP1/hash_table_trie.c
+++ TP1/hash_table_trie.c
@@ -2,15 +2,36 @@
 #include <stdlib.h>
 #include <hash_table_trie.h>
 	
+	
+// hash : niveau espace mémoire c'est plus économique mais temps recherche plus long
+
+// trouver une fonction hachage performante pour génèrer une clé pour le couple
+
+
 Trie createTrie(int maxNode) {
+	Trie t = (Trie) malloc(sizeof(struct _trie));
+	if (t == NULL) {
+		perror("malloc");
+		return NULL;
+	}
+	t->maxNode = maxNode + 1;
+	t->nextNode = 1;
+	t->transition = (List *) calloc((size_t) (maxNode) + 1, sizeof(int *));
+	// a chaque couple une fonction math qui génèe une clé
+	
+	// il peut avoir des conflits qui correspondent à 2 tables
 	return NULL;
 }
 	
 void insertInTrie(Trie trie, unsigned char *w) {
-	
+	 // applique la fonction de hash si pour cette key, il y a un maillon
 }
 	
 int isInTrie(Trie trie, unsigned char *w) {
+	
+	//  applique la fonction de hash si pour cette key, si y'a un maillon qui correspond au couple 
+	
+	
 	return 0;
 }
 	
--- TP1/transition_matrix_trie.c
+++ TP1/transition_matrix_trie.c
@@ -1,20 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <limits.h>
 #include <transition_matrix_trie.h>
+
+void printTransition(Trie trie);
+
+#define LENGTH_ASCII_CHARS UCHAR_MAX + 1
+
+// demander comment construit matrice transition
 	
 Trie createTrie(int maxNode) {
-	return NULL;
+	Trie t = (Trie) malloc(sizeof(struct _trie));
+	if (t == NULL) {
+		perror("malloc");
+		return NULL;
+	}
+	t->maxNode = maxNode + 1; // somme des longeurs des noeuds + 1 pour la racine
+	t->nextNode = 1;
+	t->transition = (int **) calloc((size_t) (maxNode) + 1, sizeof(int *));
+	if (t->transition == NULL) {
+		perror("calloc");
+		free(t->transition);
+		return NULL;
+	}
+	for (size_t i = 0; i < (size_t) t->maxNode; i++) {
+		t->transition[i] = (int *) calloc(LENGTH_ASCII_CHARS, sizeof(int));
+		if (t->transition[i] == NULL) {
+			perror("calloc");
+			free(t->transition);
+			return NULL;
+		}
+	}
+	// +1 : '\0'
+	size_t len_finite = (size_t) t->maxNode + 1;
+	t->finite = (char *) calloc(len_finite, sizeof(char));
+	if (t->finite == NULL) {
+		perror("calloc");
+		free(t->finite);
+		return NULL;
+	}
+	memset(t->finite, '0', len_finite);
+	return t;
 }
 	
 void insertInTrie(Trie trie, unsigned char *w) {
-	
+	size_t n = 0;
+	for (size_t i = 0; i < (size_t) trie->maxNode; i++) {
+		for (size_t j = 0; j < LENGTH_ASCII_CHARS; j++) {
+			if (j == w[n]) {
+				trie->transition[i][j] = trie->nextNode++;
+				break;
+			}
+		}
+		if (w[n+1] == '\0') {
+			break;
+		}
+		n++;
+	}
+	trie->finite[n] = '1';
 }
 	
 int isInTrie(Trie trie, unsigned char *w) {
-	return 0;
+	int n = -1;
+	int idx_w = 0;
+	for (int i = 0; i < trie->maxNode; i++) {
+		for (size_t j = 0; j < LENGTH_ASCII_CHARS; j++) {
+			if (trie->transition[i][j] != 0 && w[idx_w] == j) {
+				n++;
+				idx_w++;
+			}
+		}
+	}
+	printf("n : %d\n", n);
+	return n != -1 && trie->finite[n] == '1';
 }
-	
-int main(void) {
 
+void freeTrie(Trie t) {
+	if (t != NULL) {
+		for (size_t i = 0; i < (size_t) t->maxNode; i++) {
+			free(t->transition[i]);
+		}
+		free(t->transition);
+		free(t->finite);
+	}
+}
+
+void printTransition(Trie trie) {
+	for (size_t i = 0; i < (size_t) trie->maxNode; i++) {
+		printf("%u:\n", (int) i);
+		for (size_t j = 0; j < LENGTH_ASCII_CHARS; j++) {
+			if (isprint(j)) {
+				printf("printable char(ascii:%lu) %c : %d\n", j, (char) j, trie->transition[i][j]);
+			} else {
+				printf("non printable char(ascii:%lu) : %d\n", j, trie->transition[i][j]);
+			}
+		}
+		printf("\n");
+	}
+	printf("finite :\n");
+	for (size_t i = 0; i < (size_t) trie->maxNode; i++) {
+		printf("%c|", trie->finite[i]);
+	}
+	printf("\n");
+}
+
+int main(void) {
+	int maxNode = 3;
+	Trie trie = createTrie(maxNode);
+	if (trie == NULL) {
+		return EXIT_FAILURE;
+	}
+	insertInTrie(trie, (unsigned char *) "atcg");
+	printTransition(trie);
+	if (isInTrie(trie, (unsigned char *) "tc")) {
+		printf("Word %s is in trie\n", "tc");
+	} else {
+		printf("Word %s isn't in trie\n", "tc");
+	}
+	freeTrie(trie);
 	return EXIT_SUCCESS;
 }
