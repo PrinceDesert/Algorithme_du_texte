e7ce2c69e95ca841d39422cd03e06ffc0ee2d93c
DesertPrince <lepetitprince10@gmx.fr>
2022-10-15 10:32:21 +0200

    update

--- TP2/exercice.c
+++ TP2/exercice.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * Algorithmes de recherche exacte d’un mot dans un texte
+*/
+
 // avec sentinelle : concatene le mot à la fin
 
 // trop à droite : si le mot a comparé dépasse la fin du texte
@@ -9,57 +13,62 @@
 // boucle rapide : décale la fenetre à la premier occurence de la lettre en gros c'est l'index qui se décale à une potentiel occurence
 
 // algorithme naif, avec boucle interne, sans boucle rapide, sans sentinelle (word = mot à trouver) pour compter le nb d'occurence de word dans text
-int algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m);
+size_t algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m);
 // algorithme naif, avec boucle interne, avec boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text
-int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m);
+size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m);
 // algorithme naif, avec boucle interne, avec boucle rapide, avec sentinelle pour compter le nb d'occurence de word dans text
-int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char *text, int n, char *word, int m);
-// algorithme naif, avec strncmp, sans boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text 
-int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char *text, int n, char *word, int m);
-// algorithme naif, avec strncmp, sans boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text 
-int algorithme_naif_avec_strncmp_sans_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m);
-// algorithme naif, avec strncmp, avec boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text 
-int algorithme_naif_avec_strncmp_avec_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m);
-// algorithme naif, avec strncmp, avec boucle rapide, avec sentinelle pour compter le nb d'occurence de word dans text
-int algorithme_naif_avec_strncmp_avec_boucle_rapide_avec_sentinelle(char *text, int n, char *word, int m);
+size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char *text, size_t n, const char *word, size_t m);
 
-int findNextIndex(char *word, size_t word_len, char c);
+// algorithme naif, avec strncmp, sans boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text 
+size_t algorithme_naif_avec_strncmp_sans_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m);
+// algorithme naif, avec strncmp, avec boucle rapide, sans sentinelle pour compter le nb d'occurence de word dans text 
+//size_t algorithme_naif_avec_strncmp_avec_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m);
+// algorithme naif, avec strncmp, avec boucle rapide, avec sentinelle pour compter le nb d'occurence de word dans text
+//size_t algorithme_naif_avec_strncmp_avec_boucle_rapide_avec_sentinelle(char *text, size_t n, const char *word, size_t m);
 
 // algorithme de Morris-Pratt
+size_t algorithme_Morris_Pratt();
 // algorithme de Knuth-Morris-Pratt
+size_t algorithme_Knuth_Morris_Pratt();
 // algorithme de Boyer-Moore
+size_t algorithme_Boyer_Moore();
 // algorithme de Horspool
+size_t algorithme_Horspool();
 // algorithme Quick Search
+size_t algorithme_Quick_Search();
 
-/** 
- * premier genérateur pseudo-aléatoire de texte permettant de stocker
-dans des fichiers des textes d’une longueur donn ́ee sur un alphabet de taille
-donnée
-*/
+// utils
+int findNextIndex(const char *word, size_t word_len, char c);
+char *substr(const char *src, size_t pos, size_t len);
+size_t brute_force_search(const char *x, size_t m, const char *y, size_t n);
 
 // un generateur de texte, un generateur de mot en bash
 // generer les courbes avec un autre outil
 
-int findNextIndex(char *word, size_t word_len, char c) {
-	if (word == NULL || word_len == 0) return -1;
-	for (int i = 0; i < word_len; i++) {
-		if (word[i] == c) {
-			return i;
-		}
-	}
-	return -1;
+int main(void) {
+	const char *x = "ataatata";
+	const char *y = "ataataatataatataa";
+	
+	printf("== brute_force_search : %lu ==\n",
+		brute_force_search(x, strlen(x), y, strlen(y)));
+	
+	printf("== algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle : %lu ==\n",
+		algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(x, strlen(x), y, strlen(y)));
+	
+	
+	return EXIT_SUCCESS;
 }
 
-size_t algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(char *text, size_t n, char *word, size_t m) {
+size_t algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m) {
 	if (text == NULL || word == NULL || n == 0 || m == 0) return 0;
-	int i, j;
-	int nbOcc = 0;
+	size_t i, j;
+	size_t nbOcc = 0;
 	for (j = 0; j <= n - m; ++j) {
 		for (i = 0; i < m && text[i] == word[i + j]; ++i);
 		if (i >= m) {
-			printf("shift : %d\n", j);
+			printf("shift : %lu\n", j);
 			printf("%s\n", word);
-			printf("%*s%s\n", j, " ", text);
+			printf("%*s%s\n", (int) j, " ", text);
 			nbOcc++;
 		}
 	}
@@ -67,21 +76,21 @@ size_t algorithme_naif_avec_boucle_interne_sans_boucle_rapide_sans_sentinelle(ch
 }
 
 // utilise findNextInd pour passer à la prochaine occurence pour décaler la fenetre jusqu'a la prochaine occurence trouve ( = boucle rapide)
-size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_sans_sentinelle(char *text, size_t n, char *word, size_t m) {
+size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m) {
 	if (text == NULL || word == NULL || n == 0 || m == 0) return 0;
-	int i, j;
-	int nbOcc = 0;
+	size_t i, j;
+	size_t nbOcc = 0;
 	int nextOcc = -1;
 	for (j = 0; j <= n - m; ++j) {
 		for (i = 0; i < m && text[i] == word[i + j]; ++i);
 		if (i >= m) {
-			printf("shift : %d\n", j);
+			printf("shift : %lu\n", j);
 			printf("%s\n", word);
-			printf("%*s%s\n", j, " ", text);
+			printf("%*s%s\n", (int) j, " ", text);
 			nbOcc++;
 		} else {
-			if ((nextOcc = findNextIndex(text, word[i])) != -1) {
-				j = nextOcc;
+			if ((nextOcc = findNextIndex(text, strlen(text), word[i])) != -1) {
+				j = (size_t) nextOcc;
 			}
 		}
 	}
@@ -89,14 +98,14 @@ size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_sans_sentinelle(ch
 }
 
 // la sentinelle sert à ne pas tester à chaque fois si on est en fin de mot
-int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char *text, size_t n, char *word, size_t m) {
-	int i, j = 0;
-	int nbOcc = 0;
+size_t algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char *text, size_t n, const char *word, size_t m) {
+	size_t i, j = 0;
+	size_t nbOcc = 0;
 	int nextOcc = -1;
 	
 	// on ajoute le mot à la fin pour être sur d'avoir une occurence = sentinelle
 	size_t newTextLength = n + strlen(word);
-	text = (char *) realloc(newTextLength * sizeof(char));
+	text = (char *) realloc(text, newTextLength * sizeof(char));
 	if (text == NULL) {
 		perror("realloc");
 		exit(EXIT_FAILURE);
@@ -109,13 +118,13 @@ int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char 
 	while (nbOcc != 1) {
 		for (i = 0; i < m && text[i] == word[i + j]; ++i);
 		if (i >= m) {
-			printf("shift : %d\n", j);
+			printf("shift : %lu\n", j);
 			printf("%s\n", word);
-			printf("%*s%s\n", j, " ", text);
+			printf("%*s%s\n", (int) j, " ", text);
 			nbOcc++;
 		} else {
-			if ((nextOcc = findNextIndex(text, word[i])) != -1) {
-				j = nextOcc;
+			if ((nextOcc = findNextIndex(text, strlen(text), word[i])) != -1) {
+				j = (size_t) nextOcc;
 			}
 		}
 	}
@@ -123,37 +132,50 @@ int algorithme_naif_avec_boucle_interne_avec_boucle_rapide_avec_sentinelle(char 
 }
 
 // avec strncmp au lieu de comparer 1 par 1, on utilise strncmp mais on garde la boucle principale
-int algorithme_naif_avec_strncmp_sans_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m) {
+size_t algorithme_naif_avec_strncmp_sans_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m) {
 	if (text == NULL || word == NULL || n == 0 || m == 0) return 0;
-	int i, j;
-	int nbOcc = 0;
+	size_t i, j;
+	size_t nbOcc = 0;
 	for (j = 0; j <= n - m; ++j) {
 		// Extraction de la fenêtre contenant le mot du texte
-		const char *text_substr = substr(text, j, j+strlen(word));
+		const char *text_substr = substr(text, j, strlen(word)+j);
 		if (text_substr == NULL) {
-			fprintf("Erreur substring(%s,%d,%d) = NULL", word, i, j);
+			fprintf(stderr, "Error substr(%s,%lu,%lu) = NULL", word, i, j);
 			exit(EXIT_FAILURE);
 		}
-		
 		if (strncmp(text_substr, word, strlen(word)) == 0) {
 			nbOcc++;
 		}
 	}
 	return nbOcc;
 }
+/*
+size_t algorithme_naif_avec_strncmp_avec_boucle_rapide_sans_sentinelle(const char *text, size_t n, const char *word, size_t m) {
+	return 0;
+}
 
-int algorithme_naif_avec_strncmp_avec_boucle_rapide_sans_sentinelle(char *text, int n, char *word, int m) {
-	
+size_t algorithme_naif_avec_strncmp_avec_boucle_rapide_avec_sentinelle(char *text, size_t n, const char *word, size_t m) {
+	return 0;
+}*/
+
+int findNextIndex(const char *word, size_t word_len, char c) {
+	if (word == NULL || word_len == 0) return -1;
+	for (size_t i = 0; i < word_len; i++) {
+		if (word[i] == c) {
+			return (int) i;
+		}
+	}
+	return -1;
 }
 
 /**
  * Substring with index
  * url : https://www.developpez.net/forums/d115796/c-cpp/c/equivalent-substring-c/
 */
-char *substr(char *src, int pos, int len) {
+char *substr(const char *src, size_t pos, size_t len) {
 	char *dest=NULL;
 	if (len>0) {
-		dest = calloc(len+1, 1);
+		dest = (char *) calloc(len+1, 1);
 		if (NULL != dest) {
 			strncat(dest, src+pos,len);
 		} else {
@@ -164,22 +186,17 @@ char *substr(char *src, int pos, int len) {
 	return dest;
 }
 
-int main()
-{
-	char *x = "ataatata";
-	char *y = "ataataatataatataa";
-	brute_force_search(x, strlen(x), y, strlen(y));
-	return EXIT_SUCCESS;
-}
-
-void brute_force_search(char *x, int m, char *y, int n) {
-	int i, j;
+size_t brute_force_search(const char *x, size_t m, const char *y, size_t n) {
+	size_t i, j;
+	size_t nbOcc = 0;
 	for (j = 0; j <= n - m; ++j) {
 		for (i = 0; i < m && x[i] == y[i + j]; ++i);
 		if (i >= m) {
-			printf("shift : %d\n", j);
+			nbOcc++;
+			printf("shift : %lu\n", j);
 			printf("%s\n", y);
-			printf("%*s%s\n", j, " ", x);
+			printf("%*s%s\n", (int) j, " ", x);
 		}
 	}
+	return nbOcc;
 }
--- TP2/text_generator.c
+++ TP2/text_generator.c
@@ -1,47 +1,81 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <assert.h>
 
 #define FILENAME "./text.txt"
 
-void generator1(size_t length);
-
 /**
- * générateur pseudo-aléatoire de texte permettant de stocker
+ * Écrire un premier générateur pseudo-aléatoire de texte permettant de stocker
  * dans des fichiers des textes d’une longueur donnée sur un alphabet de taille donnée.
  * Utiliser ce générateur pour produire des textes de longueur 500 000 sur des alphabets de taille 2, 4, 20 et 70.
 */
-void generator1(size_t length) {
-	FILE *fout = fopen(FILENAME, "wb");
+void generator1(const char *filename, size_t length_text, size_t length_alphabet);
+
+/**
+ * Écrire un second générateur pseudo-aléatoire de mots permettant de stocker dans des fichiers un nombre de mots donné, d’une longueur donnée sur un alphabet de taille donnée.
+ * Utiliser ce générateur pour produire des ensembles de 100 mots de longueur 4, 5, 6, 7, 8, 9, 10, 15, 20, 30, 40 et 50 sur des alphabets de taille 2, 4, 20 et 70.
+*/
+void generator2(const char *filename, size_t nb_wordn size_t length_word, size_t length_alphabet, const *char separator);
+
+void generator1(const char *filename, size_t length_text, size_t length_alphabet) {
+	assert(filename != NULL);
+	assert(length_text > 0);
+	assert(length_alphabet >= 2 && length_alphabet <= 70);
+	FILE *fout = fopen(filename, "wb");
 	if (fout == NULL) {
-		fprintf(stderr, "Impossible to open %s to write\n", FILENAME);
+		fprintf(stderr, "Cannot open %s to write\n", filename);
 		return;
 	}
-	// A-Z : 65-90
-	// a-z : 97-122
-	// rand() % (max + 1 - min) + min
-	// rand to choose upper or lower letters
-	short rL = rand() % 2;
-	char a = 'A';
-	char z = 'Z';
-	if (rL) {
-		a = 'A';
-		z = 'Z';
-	}
-	int i = 0;
+	size_t min = (size_t) '!';
+	size_t max = min + length_alphabet;
 	char c = '\0';
-	while (i++ < length) {
-		c = (rand() % ((((int) z) + 1) - ((int) a)) + ((int) a));
+	size_t i = 0;
+	while (i++ < length_text) {
+		c = (char) ((size_t) rand() % ((max + 1) - min) + min);
 		fprintf(fout, "%c", c);
 	}
 	if (fclose(fout) != 0) {
-		fprintf(stderr, "Cannot close %s\n", FILENAME);
+		fprintf(stderr, "Cannot close %s\n", filename);
+		return;
+	}
+	fprintf(stdout, "Text generated in %s\n", filename);
+}
+
+
+void generator2(const char *filename, size_t nb_word, size_t length_word, size_t length_alphabet, const *char separator) {
+	assert(filename != NULL);
+	assert(length_word >= 4 && length_word <= 40);
+	assert(length_alphabet >= 2 && length_alphabet <= 70);
+	assert(separator != NULL);
+	FILE *fout = fopen(filename, "wb");
+	if (fout == NULL) {
+		fprintf(stderr, "Cannot open %s to write\n", filename);
+		return;
+	}
+	size_t min = (size_t) '!';
+	size_t max = min + length_alphabet;
+	char c = '\0';
+	size_t i = 0;
+	size_t j = 0;
+	while (i++ < nb_word) {
+		char word[length_word];
+		while (j++ < length_word) {
+			c = (char) ((size_t) rand() % ((max + 1) - min) + min);
+			fprintf(fout, "%c", c);
+		}
+		fprintf(fout, "%s", separator);
+		j = 0;
+	}
+	if (fclose(fout) != 0) {
+		fprintf(stderr, "Cannot close %s\n", filename);
 		return;
 	}
 }
 
-int main() {
-	size_t length = length;
-	generator1(500000);
+int main(void) {
+	size_t length_text = 500000;
+	size_t length_alphabet = 70;
+	generator1(FILENAME, length_text, length_alphabet);
 	return EXIT_SUCCESS;
 }
