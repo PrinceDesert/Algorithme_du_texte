eb56cccf20f80dce67a8487434177a62ef96cabb
DesertPrince <lepetitprince10@gmx.fr>
2022-11-17 17:06:34 +0100

    update

--- TP3/ac-matrice.c
+++ TP3/ac-matrice.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
-int ac_matrice(const char *filename_word, const char *filename_text);
+int ac_matrice(const char *filename_words, const char *filename_text);
+
+#define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]))
 
 int main(void) {
+	
+	ac_matrice("./mots.txt", "./texte.txt");
 	
 	return EXIT_SUCCESS;
 }
 
-int ac_matrice(const char *filename_word, const char *filename_text) {
+int ac_matrice(const char *filename_words, __attribute__((unused)) const char *filename_text) {
+	
+	
+	// pour chaque mot
+	FILE *fp;
+	int size = 256;
+	char line[size];
+	
+	fp = fopen(filename_words, "rb");
+	if (!fp) {
+		fprintf(stderr, "Cannot open %s to read\n", filename_words);
+		exit(EXIT_FAILURE);
+	}
+	
+	while (!feof(fp)) {
+		if (fgets(line, ARRAY_SIZE(line), fp) == NULL) {
+			if (ferror(fp)) {
+				fprintf(stderr, "fgets failed : %d\n", errno);
+				exit(EXIT_FAILURE);
+			}
+		}
+		line[strcspn(line, "\n")] = '\0';
+		printf("line : %s\n", line);
+		memset(line, '\0', sizeof(line));
+	}
+	// Algo AC
+	
+	
+	
+	if (fclose(fp) != 0) {
+		fprintf(stderr, "Cannot close %s\n", filename_words);
+		exit(EXIT_FAILURE);
+	}
 	
 	return 0;
 }
 
+/*
 void pre_ac(const char *x, int k) {
 	Trie trie = createTrie(maxNode);
 	if (trie == NULL) {
@@ -20,7 +59,7 @@ void pre_ac(const char *x, int k) {
 	}
 	char *q0 = "";
 	// for i 
-	insertInTrie(trie, q0);
+	insertInTrie(trie, x);
 	
 }
 
@@ -53,6 +92,7 @@ int completer(int e) {
 	
 	// }
 }
+*/
 
 // La liste des transitions
 // struct transition {int startNode, int endNode, char letter}
@@ -64,3 +104,4 @@ int completer(int e) {
 // enfiler, defiler
 
 // la fonction sup regarde le plus long suffixe pour trouver l'état où atterir
+
